102f4e14a227f05b14bdb4756da17b556db30e51
Ara Badalyan <arabadalyan@yahoo.com>
2022-03-22 08:00:20 +0400

    Initial commit

---
+++ app/src/main/java/com/hundredstartups/openvpn/interfaces/ChangeServer.java
@@ -0,0 +1,7 @@
+package com.hundredstartups.openvpn.interfaces;
+
+import com.hundredstartups.openvpn.model.Server;
+
+public interface ChangeServer {
+    void newServer(Server server);
+}
---
+++ app/src/main/java/com/hundredstartups/openvpn/interfaces/NavItemClickListener.java
@@ -0,0 +1,5 @@
+package com.hundredstartups.openvpn.interfaces;
+
+public interface NavItemClickListener {
+    void clickedItem(int index);
+}
